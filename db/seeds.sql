a6aa71aa2cd8c8dd9b010e5af477f21ba1ca58ef
randze <65006180+randze@users.noreply.github.com>
2020-07-02 22:07:48 -0400

    Added USE db

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,5 @@
+USE burgers_db;
+
 INSERT INTO burgers VALUES(0, "Random Burger", true, default);
 INSERT INTO burgers VALUES(0, "Double Cheese Burger", false, default);
 INSERT INTO burgers VALUES(0, "MCDOUBLE WITH MCSAUCE", false, default);
